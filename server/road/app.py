cf85121c66a99b312c47c38a2273556c5ad8b7d7
Yaniv <yanivdouieb@icloud.com>
2025-04-22 19:21:24 +0200

    Integrate custom OpenAPI parameters into FastAPI app

--- server/road/app.py
+++ server/road/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from road.openapi import OPENAPI_PARAMETERS
+
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(
+        **OPENAPI_PARAMETERS
+    )
     return app
 
 app = create_app()
